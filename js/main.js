9a4496af9c2923e363039002b538d09cd8e48ccf
Val <valentinedestrait@hotmail.com>
2020-01-20 16:28:24 +0100

    Mobile

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ const year = date.getFullYear();
 const spanElement = document.getElementById("current-year");
 spanElement.innerText = year;
 
+// Dynamic margin for main tag
+$(document).ready(function() {
+  const headerHeight = $('header').height();
+  console.log(headerHeight);
+  $('main').css({marginTop: headerHeight});
+});
+
 // Roll the heads
-console.log(window.innerWidth);
-console.log("Height :" + window.innerHeight);
 $(document).ready(function() {
   window.onscroll = function() {
     animateHeads();
@@ -14,15 +19,29 @@ $(document).ready(function() {
 
   function animateHeads() {
     const heads = document.getElementsByClassName("head");
-    //console.log(heads);
     for (let i = 0; i < heads.length; i++) {
       const bounding = heads[i].getBoundingClientRect();
-      console.log(bounding.y);
       if (bounding.y < window.innerHeight * 0.75) {
-        console.log(bounding.y);
-        console.log("Height :" + window.innerHeight * 0.5);
         $(heads[i]).addClass("animated rollIn");
       }
     }
+  }
+});
+
+// Mobile Slide-In #competences
+$(document).ready(function() {
+  if (window.innerWidth < 767){
+    $('#competences').on('click', function(){
+      if($(this).hasClass('open')){
+        $(this).removeClass('open');
+        $(this).find('#open-icon').text('keyboard_arrow_right');
+      } else {
+        $(this).addClass('open');
+        $(this).find('#open-icon').text('close');
+        setTimeout(function(){ 
+          $('.head').addClass("animated rollIn");
+        }, 500);
+      }
+    });
   }
 });
