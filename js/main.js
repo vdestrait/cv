11f95d84959fb393001f893ce681ce432104d6d5
Val <valentinedestrait@hotmail.com>
2020-01-20 21:41:56 +0100

    competences

--- js/main.js
+++ js/main.js
@@ -4,11 +4,36 @@ const year = date.getFullYear();
 const spanElement = document.getElementById("current-year");
 spanElement.innerText = year;
 
+// Menu Slides
+$(document).ready(function() {
+  $("header .material-icons").on("click", function() {
+    if ($(this).hasClass("open-menu")) {
+      $(this).removeClass("open-menu");
+      $(this).text("subject");
+      $(this)
+        .next()
+        .slideDown();
+      $(this)
+        .siblings("nav")
+        .slideUp();
+    } else {
+      $(this)
+        .next()
+        .slideUp();
+      $(this)
+        .siblings("nav")
+        .slideDown();
+      $(this)
+        .text("close")
+        .addClass("open-menu");
+    }
+  });
+});
+
 // Dynamic margin for main tag
 $(document).ready(function() {
-  const headerHeight = $('header').height();
-  console.log(headerHeight);
-  $('main').css({marginTop: headerHeight});
+  const headerHeight = $("header").height();
+  $("main").css({ paddingTop: headerHeight });
 });
 
 // Roll the heads
@@ -30,16 +55,20 @@ $(document).ready(function() {
 
 // Mobile Slide-In #competences
 $(document).ready(function() {
-  if (window.innerWidth < 767){
-    $('#competences').on('click', function(){
-      if($(this).hasClass('open')){
-        $(this).removeClass('open');
-        $(this).find('#open-icon').text('keyboard_arrow_right');
+  if (window.innerWidth < 767) {
+    $("#competences").on("click", function() {
+      if ($(this).hasClass("open")) {
+        $(this).removeClass("open");
+        $(this)
+          .find("#open-icon")
+          .text("keyboard_arrow_right");
       } else {
-        $(this).addClass('open');
-        $(this).find('#open-icon').text('close');
-        setTimeout(function(){ 
-          $('.head').addClass("animated rollIn");
+        $(this).addClass("open");
+        $(this)
+          .find("#open-icon")
+          .text("close");
+        setTimeout(function() {
+          $(".head").addClass("animated rollIn");
         }, 500);
       }
     });
